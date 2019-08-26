ab0bdeb081b13fd7217bc786c0f73970a0e73978
Andrew <andrewpeliza@hotmail.com>
2019-08-26 09:55:57 +0100

    Passing information down from screen to screen / also to header.

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Platform } from 'react-native';
+
+import Colors from '../constants/Colors';
+import { CATEGORIES } from '../data/dummy-data';
 
 const CategoryMealsScreen = props => {
+  const catId = props.navigation.getParam('categoryId');
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
   return (
     <View style={styles.screen}>
       <Text>The Category Meals Screen!</Text>
+      <Text>{selectedCategory.title}</Text>
       <Button
         title='Go to Details'
         onPress={() => {
@@ -21,6 +28,19 @@ const CategoryMealsScreen = props => {
       />
     </View>
   );
+};
+
+CategoryMealsScreen.navigationOptions = navigationData => {
+  const catId = navigationData.navigation.getParam('categoryId');
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+  return {
+    headerTitle: selectedCategory.title,
+    headerStyle: {
+      backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+  };
 };
 
 const styles = StyleSheet.create({
