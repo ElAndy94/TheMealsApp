b4c356152d54ee3c3a20c86ec3cf95ec9ff38012
Andrew <andrewpeliza@hotmail.com>
2019-09-16 23:16:26 +0100

    adding a model to the meals, dummy data to play with and displaying it.

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 
-import { CATEGORIES } from '../data/dummy-data';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
 
 const CategoryMealsScreen = props => {
+  const renderMealItem = itemData => {
+    return (
+      <View>
+        <Text>{itemData.item.title}</Text>
+      </View>
+    );
+  };
+
   const catId = props.navigation.getParam('categoryId');
-  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+  const displayedMeals = MEALS.filter(
+    meal => meal.categoryIds.indexOf(catId) >= 0
+  );
+  // const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
 
   return (
     <View style={styles.screen}>
-      <Text>The Category Meals Screen!</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem}
+      />
+      {/* <Text>The Category Meals Screen!</Text>
       <Text>{selectedCategory.title}</Text>
       <Button
         title='Go to Details'
@@ -24,7 +41,7 @@ const CategoryMealsScreen = props => {
         onPress={() => {
           props.navigation.goBack();
         }}
-      />
+      /> */}
     </View>
   );
 };
